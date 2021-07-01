c3a3068cbdd6b614e8d66e02604582aa125c32ae
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-07-01 06:55:09 +0530

    wrapped app component with context, chakra and router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { GlobalContextProvider } from "./context-api/GlobalContext";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <GlobalContextProvider>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </GlobalContextProvider>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
