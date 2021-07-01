9d218f8a222f36cf95d10849d0a8ba9ae1240de1
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-07-01 06:57:07 +0530

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Home from "./quiz/pages/Home";
+import Loader from "./shared/components/Loader";
+import { useGlobalContext } from "./context-api/GlobalContext";
+import { Routes, Route } from "react-router-dom";
 function App() {
+  // const { loading, questions } = useGlobalContext();
+  // console.log(questions);
   return (
-    <div className="App">
-      <header className="App-header">
-        My Quiz App
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
