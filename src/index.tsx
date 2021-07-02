1f25e5f997ee7ce69b0cff9b9ad7f56bacaea58c
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-07-02 23:07:13 +0530

    feat: auth0 setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { GlobalContextProvider } from "./context-api/GlobalContext";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <GlobalContextProvider>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-      </GlobalContextProvider>
-    </Router>
+    <Auth0Provider
+      domain="dev-p5e8shnz.us.auth0.com"
+      clientId="P1Dvcls6tgBIbZUI2tuxF3TGEx6d0AmF"
+      redirectUri={window.location.origin}
+    >
+      <Router>
+        <GlobalContextProvider>
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </GlobalContextProvider>
+      </Router>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Text } from "@chakra-ui/react";
+import { Avatar, WrapItem } from "@chakra-ui/react";
 
 function Navbar() {
-  return <nav className="head-section__nav">This is navbar</nav>;
+  const { user, isAuthenticated, logout, loginWithRedirect } = useAuth0<any>();
+  console.log(user);
+  return (
+    <nav className="head-section__nav">
+      {!isAuthenticated && (
+        <div className="title">
+          <Text fontSize="2xl">C Quiz</Text>
+        </div>
+      )}
+
+      <div className="user-info">
+        <WrapItem>
+          {isAuthenticated && user.picture && (
+            <Avatar name={user.nickname} src={user.picture} />
+          )}
+        </WrapItem>
+        {isAuthenticated && user.nickname && (
+          <Text fontSize="1xl" ml="0.5rem">
+            {`Welcome, `}
+            <strong>{`${user.name.toUpperCase()}`}</strong>
+          </Text>
+        )}
+      </div>
+      <div className="access-control">
+        {!isAuthenticated && (
+          <button onClick={() => loginWithRedirect()}>
+            <i className="fas fa-sign-in-alt"></i>
+            {` Log In`}
+          </button>
+        )}
+        {"  "}
+        {isAuthenticated && (
+          <button onClick={() => logout({ returnTo: window.location.origin })}>
+            <i className="fas fa-sign-out-alt"></i>
+            {` Log Out`}
+          </button>
+        )}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
